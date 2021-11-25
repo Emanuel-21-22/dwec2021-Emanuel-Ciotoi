927d10b674a4625599709275b0cd0fa722783610
Emanuel <emanuel12m@gmail.com>
2021-11-25 18:39:15 +0100

    tu18 done

---
+++ docs/tu18/scripts/index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const div = document.body.firstElementChild;
+
+console.log(div);
+
+const ul = div.nextElementSibling;
+const liPete = ul.lastElementChild;
+
+console.log(ul);
+console.log(liPete);
